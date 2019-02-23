e70c5e6077fc8e13f20a4a8886df1e2a12d2e227
efancher <efan@cloudera.com>
2019-02-22 16:42:20 -0800

    Thompson working in loop.

--- Agent.jl
+++ Agent.jl
@@ -1,14 +1,17 @@
 module PFAgent
+    using POMDPs
     using POMDPPolicies
     using POMDPSimulators
     curry(f, x) = (xs...) -> f(x, xs...)
-    function run_chain!(;policies, found_target, true_mdp, update_Q, n_agents, n_states,
+    function run_chain!(;policies, found_target, mdps, update_Q, n_agents, n_states,
                         Q_tables, N_tables, epochs, steps, rev_action_map, stop_early)
+        r_history =[]
         for e in 1:epochs
             agents = []
             for i in 1:n_agents
                 push!(agents,
-                      Iterators.Stateful(stepthrough(true_mdp, FunctionPolicy(policies[i]), "s,a,r,sp,t", max_steps=steps)))
+                      Iterators.Stateful(stepthrough(mdps[i], FunctionPolicy(policies[i]), "s,a,r,sp,t",
+                      max_steps=steps)))
             end
 
             #println("epoch: $e")
@@ -29,15 +32,19 @@ module PFAgent
                     end
                     res = popfirst!(agents[i])
                     r = res[:r]
-                    N_tables[i][res[:s],rev_action_map[res[:a]]] += 1
-                    update_Q(Q_tables[i],res..., rev_action_map)
                     t = res[:t]
-                    if e % 10 == 0 || stop_early
-                      println("e: $e, t: $t, print agent $i result: $res")
+                    push!(r_history, (e,i,t,r))
+                    # println("before: i:$i, s:$(res[:s]), a:$(rev_action_map[res[:a]]), N:$(N_tables[i][res[:s],rev_action_map[res[:a]]])")
+                    N_tables[i][res[:s],rev_action_map[res[:a]]] += 1
+                    # println("after: i:$i, s:$(res[:s]), a:$(rev_action_map[res[:a]]), N:$(N_tables[i][res[:s],rev_action_map[res[:a]]])")
+
+                    update_Q(Q_tables[i],res..., rev_action_map)
+                    if e % (floor(epochs/10)) == 0 || stop_early
+                       println("e: $e, t: $t, agent $i, result: $res")
                     end
-                    if found_target(r) && stop_early
-                        return "Done!"
-                    end
+                    # if found_target(r) && stop_early
+                    #     return "Done!"
+                    # end
                end
                for i in 1:n_agents
                     if ! isempty(agents[i])
@@ -46,11 +53,12 @@ module PFAgent
                end
             end
         end
+        return r_history
     end
-    function setup_agents(states, num_states, num_agents, actions, num_actions, policy_function, AGMDP)
+    function setup_agents(states, num_states, num_agents, actions, num_actions, policy_function)
         Q_tables = []
         policies = []
-        mdps = Any[]
+        println("setup agents")
         empty_N = zeros( num_states+2, num_actions)
         N_tables = Dict{Int32, typeof(empty_N)}()
         Central_Q_table = Dict{Int32, Dict{Int32, Float32}}()
@@ -61,13 +69,12 @@ module PFAgent
               Q_tables[i][state] = Dict{Int32, Float32}()
               # print(Q_tables[i])
               for action in actions
-                 Q_tables[i][state][action] = 0
+                 Q_tables[i][state][action] = 0.0
               end
             end
             N_tables[i] = deepcopy(empty_N)
             push!(policies, curry(curry(curry(curry(policy_function, Q_tables), N_tables),i), actions))
-            push!(mdps, AGMDP)
         end
-        return (Q_tables, N_tables, policies, mdps)
+        return (Q_tables, N_tables, policies)
     end
 end
